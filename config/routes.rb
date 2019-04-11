efbcea6b34fbd1b467c233b69995ccdb61536eea
goatcheese82 <nonsquishy@gmail.com>
2019-04-11 16:55:59 -0500

    added authentication controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
-  post '/auth/login', to: 'authentication#login'
+  post '/auth/login', to: 'authentication#authenticate'
   get '/#a', to: 'application#not_found'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
