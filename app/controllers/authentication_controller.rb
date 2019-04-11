2c15655a05d9f09d9fe1c5d62e72d3d22e760dbe
goatcheese82 <nonsquishy@gmail.com>
2019-04-11 18:44:33 -0500

    added create to users controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,6 @@
 class AuthenticationController < ApplicationController
     def authenticate
-      auth_token =
-        AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+      auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
       json_response(auth_token: auth_token)
     end
   
@@ -10,4 +9,4 @@ class AuthenticationController < ApplicationController
     def auth_params
       params.permit(:email, :password)
     end
-  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    def index
+    end
+
+    def create
+        user = User.create!(user_params)
+        auth_token = AuthenticateUser.new(user.email, user.password).call
+        response = { message: Message.account_created, auth_token: auth_token }
+        json_response(response, :created)
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+
+    def user_params
+        params.permit(
+            :first_name,
+            :last_name,
+            :email,
+            :username,
+            :password,
+            :password_confirmation
+        )
+    end
 end
